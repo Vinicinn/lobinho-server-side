0ad88a2b20aac1f0c964f7762ad946e9a5ca48f6
Vinicius Melo <viniciusmelo554@gmail.com>
2024-08-23 00:39:03 -0300

    server conectando e desconectando jogadores, salvando jogadores e prontos

--- index.js
+++ index.js
@@ -13,10 +13,49 @@ const io = new Server(server, {
   },
 });
 
+// ATRIBUTOS DO JOGO
+let configs = {};
+configs.jogadores = [];
+configs.prontos = 0;
+
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
+
+  socket.on("novoJogador", (data) => {
+    configs.jogadores.push({ id: socket.id, nome: data });
+    console.log("Jogador " + data + " entrou com id: " + socket.id);
+
+    io.emit("atualizarJogadores", configs.jogadores);
+    io.emit("jogadoresProntos", configs.prontos);
+  });
+
+  socket.on("alterarPronto", (data) => {
+    if (data) {
+      configs.jogadores.at(socket.id).pronto = true;
+      configs.prontos += 1;
+    } else {
+      configs.jogadores.at(socket.id).pronto = false;
+      configs.prontos -= 1;
+    }
+    io.emit("jogadoresProntos", configs.prontos);
+  });
+
+  socket.on("disconnect", () => {
+    if (configs.jogadores.length) {
+      const jogadorDesconectado = configs.jogadores.find(
+        (jogador) => jogador.id === socket.id
+      );
+
+      console.log("Jogador " + jogadorDesconectado.nome + " saiu.");
+      configs.jogadores = configs.jogadores.filter(
+        (jogador) => jogador.id !== socket.id
+      );
+
+      io.emit("atualizarJogadores", configs.jogadores);
+    }
+  });
 });
 
 server.listen(port, () => {
-  console.log("Example app listening on port " + port);
+  console.log(`Example app listening on port ${port}`);
 });
